b4a1f6a3054993ebf3d799b631dfd0dc62df33e9
katleese <katherine@sevenshift.de>
2026-05-20 13:54:07 +0200

    refactor: remove unused TogglAPI class (deprecated v8 endpoint)

--- togglapi/api.py
+++ togglapi/api.py
@@ -1,83 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# @author Pablo Endres <epablo+code@pabloendres.com> - ReportAPI
-#@author Mosab Ahmad <mosab.ahmad@gmail.com> - TogglAPI
+# @author Pablo Endres <epablo+code@pabloendres.com>
 
 
 from urllib.parse import urlencode
 
 import requests
 from requests.auth import HTTPBasicAuth
-
-
-class TogglAPI(object):
-    """A wrapper for Toggl Api"""
-
-    def __init__(self, api_token, timezone):
-        self.api_token = api_token
-        self.timezone = timezone
-
-    def _make_url(self, section='time_entries', params={}):
-        """Constructs and returns an api url to call with the section of the API to be called
-        and parameters defined by key/pair values in the params dict.
-        Default section is "time_entries" which evaluates to "time_entries.json"
-
-        >>> t = TogglAPI('_SECRET_TOGGLE_API_TOKEN_')
-        >>> t._make_url(section='time_entries', params = {})
-        'https://www.toggl.com/api/v8/time_entries'
-
-        >>> t = TogglAPI('_SECRET_TOGGLE_API_TOKEN_')
-        >>> t._make_url(section='time_entries', params = {'start_date' : '2010-02-05T15:42:46+02:00', 'end_date' : '2010-02-12T15:42:46+02:00'})
-        'https://www.toggl.com/api/v8/time_entries?start_date=2010-02-05T15%3A42%3A46%2B02%3A00%2B02%3A00&end_date=2010-02-12T15%3A42%3A46%2B02%3A00%2B02%3A00'
-        """
-
-        url = 'https://www.toggl.com/api/v8/{}'.format(section)
-        if len(params) > 0:
-            url = url + '?{}'.format(urlencode(params))
-        return url
-
-    def _query(self, url, method):
-        """Performs the actual call to Toggl API"""
-
-        url = url
-        headers = {'content-type': 'application/json'}
-
-        if method == 'GET':
-            return requests.get(url, headers=headers, auth=HTTPBasicAuth(self.api_token, 'api_token'))
-        elif method == 'POST':
-            return requests.post(url, headers=headers, auth=HTTPBasicAuth(self.api_token, 'api_token'))
-        else:
-            raise ValueError('Undefined HTTP method "{}"'.format(method))
-
-    ## Time Entry functions
-    def get_time_entries(self, start_date='', end_date='', timezone=''):
-        """Get Time Entries JSON object from Toggl"""
-
-        url = self._make_url(section='time_entries',
-                             params={'start_date': start_date + self.timezone, 'end_date': end_date + self.timezone})
-        r = self._query(url=url, method='GET')
-        return r.json()
-
-    def get_hours_tracked(self, start_date, end_date):
-        """Count the total tracked hours excluding any RUNNING real time tracked time entries"""
-        time_entries = self.get_time_entries(start_date=start_date.isoformat(), end_date=end_date.isoformat())
-
-        if time_entries is None:
-            return 0
-        total_seconds_tracked = sum(max(entry['duration'], 0) for entry in time_entries)
-        return (total_seconds_tracked / 60.0) / 60.0
-
-    def get_tracked_entries(self, start_date, end_date):
-        """Return the actual time entries in a list"""
-        time_entries = self.get_time_entries(start_date=start_date.isoformat(), end_date=end_date.isoformat())
-        return time_entries
-
-    def get_time_entries_sum_per_day(self, start_date='', end_date='', timezone=''):
-        """Count the total tracked hours excluding any RUNNING real time tracked time entries"""
-        time_entries = self.get_time_entries(start_date=start_date.isoformat(), end_date=end_date.isoformat())
-
-        if time_entries is None:
-            return 0
 
 
 class ReportAPI(object):
